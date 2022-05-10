76841a2fd63d89293414fbaa907b2e7c1ef14e99
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-10 20:55:21 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,50 @@
+"""
+Name: Karthik
+Date: 10th May 2022
+Day: Tuesday
+Time: 8:30-9:00PM
+File Description:  DATA TYPES
+"""
+""""Integer Data Type"""
+
+print(type(4))
+print(type(-4))
+
+a=5
+print('%i'%a)
+print('%d'%a)
+
+'''Float Data Type '''
+
+print(type(3.4))
+print(type(-0.4))
+
+b=2.4
+print('%f'%b)
+print('%.1f'%b)
+print('%.2f'%b)
+print('%9f'%b)
+
+'''String Data Type'''''''''
+
+print(type('hi'))
+print(type("hey"))
+
+c='hi'
+print('%s'%c)
+print(c,type(c))
+
+"""Boolean Data Type"""
+
+print(type(True))
+print(type(False))
+
+print(2>1)
+print(2>3)
+
+print('hey'.isidentifier())
+print('5hey'.isidentifier())
+
+
+
+
